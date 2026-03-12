7fda438beef147324d3e34c892989eb5dcaaa213
yelena touloumont <yelena.touloumont@gmail.com>
2026-03-12 19:15:53 +0100

    Created Week-3 folder

--- Week-3/display-edges.py
+++ Week-3/display-edges.py
@@ -5,16 +5,35 @@ control.initialize(exp)
 fixation = stimuli.FixCross()
 width, height = exp.screen.size
 square_size = int(width * 0.05)
-half_square = square_size // 2
+half_square = square_size//2
 left = -width // 2 + half_square
 right = width // 2 - half_square
 bottom = -height // 2 + half_square
 top = height // 2 - half_square
 
-rectangle1 = stimuli.Rectangle(size=(square_size, square_size), line_width=1, colour=(255, 0, 0), position = (left, bottom))
-rectangle2 = stimuli.Rectangle(size=(square_size, square_size), line_width=1, colour=(255, 0, 0), position = (left, top))
-rectangle3 = stimuli.Rectangle(size=(square_size, square_size), line_width=1, colour=(255, 0, 0), position = (right, bottom))
-rectangle4 = stimuli.Rectangle(size=(square_size, square_size), line_width=1, colour=(255, 0, 0), position = (right, top))
+rectangle1 = stimuli.Rectangle(
+    size=(square_size, square_size), 
+    line_width=1, 
+    colour=(255, 0, 0), 
+    position = (left, bottom)
+    )
+rectangle2 = stimuli.Rectangle(
+    size=(square_size, square_size), 
+    line_width=1, 
+    colour=(255, 0, 0), 
+    position = (left, top)
+    )
+rectangle3 = stimuli.Rectangle(
+    size=(square_size, square_size), 
+    line_width=1, colour=(255, 0, 0), 
+    position = (right, bottom)
+    )
+rectangle4 = stimuli.Rectangle(
+    size=(square_size, square_size), 
+    line_width=1, 
+    colour=(255, 0, 0), 
+    position = (right, top)
+    )
 
 control.start(subject_id=1)
 fixation.present(clear=True, update=True)
