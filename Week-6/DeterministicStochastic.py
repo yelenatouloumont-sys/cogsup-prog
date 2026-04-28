b32e2a54666db1aafd36f82a1a97b32bd7e7c864
yelena touloumont <yelena.touloumont@gmail.com>
2026-04-28 17:19:38 +0200

    Did the stochastic and deterministic exercise

--- Week-6/DeterministicStochastic.py
+++ Week-6/DeterministicStochastic.py
@@ -1,56 +1,112 @@
 from expyriment import design, control, misc, stimuli
+import random
 
 exp = design.Experiment(name="Text Experiment")
 control.set_develop_mode()
 control.initialize(exp)
 
-block_one = design.Block(name="Deterministic")
-# TRIAL 1
-trial_one = design.Trial()
-
 STIMSIZE = 100 #Size of the stimuli in pixels
 GREY = misc.constants.C_GREY #A grey color for the stimuli
 LATERAL_OFFSET = 300 #Offset for lateral positioning 
 
-square = stimuli.Rectangle(size=(STIMSIZE, STIMSIZE), colour = GREY, position=(-LATERAL_OFFSET, 0))
-circle = stimuli.Circle(radius = STIMSIZE//2, colour = GREY, position=(LATERAL_OFFSET, 0))
+# CSV FILE CULUMNS NAMES -------------------------------------------------------------------------------------------------------
+exp.data_variable_names = ["block_name", "trial_id", "target_location","key_pressed", "rt_ms"]
 
-#Preload the stimuli (this can help reduce delays during the experiment)
-square.preload()
-circle.preload()
+# BLOCK 1: DETERMINISTIC -------------------------------------------------------------------------------------------------------
+block_one = design.Block(name="Deterministic")
 
-trial_one.add_stimulus(square)
-trial_one.add_stimulus(circle)
+## TRIAL 1 : square LEFT, circle RIGHT
+trial_one = design.Trial()
+trial1_text = stimuli.TextLine(text= "TRIAL 1 - DETERMINISTIC")
 
-exp.clock.wait(2000) #Wait for 2s to let the participant see the stimuli
+rectangle_one = stimuli.Rectangle(size=(STIMSIZE, STIMSIZE), colour = GREY, position=(-LATERAL_OFFSET, 0))
+circle_one = stimuli.Circle(radius = STIMSIZE//2, colour = GREY, position=(LATERAL_OFFSET, 0))
 
-# TRIAL 2
-trial_two = design.Trial()
-stim = stimuli.TextLine(text="I am a stimulus in Block 1, Trial 2")
-trial_two.add_stimulus(stim)
+trial1_text.preload()
+rectangle_one.preload()
+circle_one.preload()
+
+trial_one.add_stimulus(trial1_text)
+trial_one.add_stimulus(rectangle_one)
+trial_one.add_stimulus(circle_one)
+
+trial_one.set_factor("square_side", "left")   #square is the target (but we can put circle also)
+
 block_one.add_trial(trial_one)
+
+## TRIAL 2 : square RIGHT, circle LEFT
+trial_two = design.Trial()
+trial2_text = stimuli.TextLine(text= "TRIAL 2 - DETERMINISTIC")
+
+rectangle_two = stimuli.Rectangle(size=(STIMSIZE, STIMSIZE), colour = GREY, position=(LATERAL_OFFSET, 0))
+circle_two = stimuli.Circle(radius = STIMSIZE//2, colour = GREY, position=(-LATERAL_OFFSET, 0))
+
+trial2_text.preload()
+rectangle_two.preload()
+circle_two.preload()
+
+trial_two.add_stimulus(trial2_text)
+trial_two.add_stimulus(rectangle_two)
+trial_two.add_stimulus(circle_two)
+
+trial_two.set_factor("square_side", "right")   #square is the target (but we can put circle also)
+
 block_one.add_trial(trial_two)
+
 exp.add_block(block_one)
 
+# BLOCK 2 : STOCHASTIC ------------------------------------------------------------------------------------------------------------------
 block_two = design.Block(name="Stochastic")
-trial_one = design.Trial()
-stim = stimuli.TextLine(text="I am a stimulus in Block 2, Trial 1")
-stim.preload()
-trial_one.add_stimulus(stim)
-trial_two = design.Trial()
-stim = stimuli.TextLine(text="I am a stimulus in Block 2, Trial 2")
-trial_two.add_stimulus(stim)
-block_two.add_trial(trial_one)
-block_two.add_trial(trial_two)
+
+## Trials are put randomly
+for i in range(4):
+    trials = design.Trial()
+
+    square_x = random.choice([-LATERAL_OFFSET, LATERAL_OFFSET]) # randomize position
+    circle_x  = LATERAL_OFFSET if square_x == -LATERAL_OFFSET else -LATERAL_OFFSET # set as other position
+    
+    stoch_text = stimuli.TextLine(text=f"TRIAL {i+1} - STOCHASTIC")
+    rectangle = stimuli.Rectangle(size=(STIMSIZE, STIMSIZE), colour=GREY, position=(square_x, 0))
+    circle = stimuli.Circle(radius=STIMSIZE // 2, colour=GREY, position=(circle_x, 0))
+
+    stoch_text.preload()
+    rectangle.preload()
+    circle.preload()
+
+    trials.add_stimulus(stoch_text)
+    trials.add_stimulus(rectangle)
+    trials.add_stimulus(circle)
+
+    trials.set_factor("square_side", "left" if square_x < 0 else "right")
+    trials.set_factor("circle_side", "left" if circle_x < 0 else "right")
+
+    block_two.add_trial(trials)
+
 exp.add_block(block_two)
+
+# START THE EXPERIMENT ------------------------------------------------------------------------------------------------------
 
 control.start()
 
 for block in exp.blocks:
     for trial in block.trials:
+
         trial.stimuli[0].present()
-        key, rt = exp.keyboard.wait([misc.constants.K_LEFT,
-                                     misc.constants.K_RIGHT])
-        exp.data.add([block.name, trial.id, key, rt])
+        exp.clock.wait(1000)
+
+        trial.stimuli[1].present(clear=True,  update=False)
+        trial.stimuli[2].present(clear=False, update=True)
+
+        #To wait for keypress
+        key, rt = exp.keyboard.wait([misc.constants.K_LEFT, misc.constants.K_RIGHT])
+
+        #Decode values for the CSV
+        target_location = trial.get_factor("square_side")  # where the square was
+        key_pressed = "left" if key == misc.constants.K_LEFT else "right"
+
+        exp.data.add([block.name, trial.id, target_location, key_pressed, rt,])
+
+        stimuli.BlankScreen().present()
+        exp.clock.wait(500)
 
 control.end()
